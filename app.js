8a41850104426795fa0be4544a88275acc236340
fladbear1979 <sfy6p390s1eh@hotmail.com>
2025-09-25 09:39:41 -0500

    Add error handling middleware to handle 404 errors

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ const userRoutes = require('./routes');
 app.use(express.json());
 app.use('/', userRoutes);
 
+// Error handling middleware
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Not Found' });
+});
+
 module.exports = app;
